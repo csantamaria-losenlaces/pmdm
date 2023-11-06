d6f9c21d00e2a5f2e71126c3e219844cebb71ad8
CarlosSantamaria <csantamaria.losenlaces@gmail.com>
2023-11-06 15:05:28 +0100

    Se ha agregado código relacionado con la actividad de la creación de la subaplicación ColorMixer empleando RecyclerView. Todavía no funcional.

--- app/src/main/java/com/csantamaria/appConMenu/colorMixer/ColorMixerActivity.kt
+++ app/src/main/java/com/csantamaria/appConMenu/colorMixer/ColorMixerActivity.kt
@@ -2,11 +2,27 @@ package com.csantamaria.appConMenu.colorMixer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.example.appmensajeria.R
 
 class ColorMixerActivity : AppCompatActivity() {
+
+    private lateinit var rvVertical: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_color_mixer)
+
+        initComponents()
+        initUI()
     }
+
+    private fun initComponents() {
+        rvVertical = findViewById<RecyclerView>(R.id.rvVertical)
+    }
+
+    private fun initUI() {
+        colorsAdapter = ColorsAdapter(colors)
+    }
+
 }
