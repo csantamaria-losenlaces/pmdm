0ef1492b1c191263f937ef6d8f55fb183b2c4dcf
CarlosSantamaria <csantamaria.losenlaces@gmail.com>
2023-11-20 17:48:05 +0100

    Terminada subaplicación ColorMixer

--- app/src/main/java/com/csantamaria/appConMenu/colorMixer/ColorMixerActivity.kt
+++ app/src/main/java/com/csantamaria/appConMenu/colorMixer/ColorMixerActivity.kt
@@ -2,7 +2,6 @@ package com.csantamaria.appConMenu.colorMixer
 
 import android.app.Dialog
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.RadioButton
 import android.widget.RadioGroup
@@ -46,9 +45,9 @@ class ColorMixerActivity : AppCompatActivity() {
             findViewById(R.id.cvH3)
         )
 
-        horizontalBars[0].setBackgroundColor(ContextCompat.getColor(this, R.color.cm_blue_20))
-        horizontalBars[1].setBackgroundColor(ContextCompat.getColor(this, R.color.cm_purple_50))
-        horizontalBars[2].setBackgroundColor(ContextCompat.getColor(this, R.color.cm_black_80))
+        horizontalBars[0].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_blue_20))
+        horizontalBars[1].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_purple_50))
+        horizontalBars[2].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_black_80))
 
         initComponents()
         initUI()
@@ -98,8 +97,97 @@ class ColorMixerActivity : AppCompatActivity() {
     }
 
     private fun updateColors(selectedBar: String, selectedColor: String) {
-        Log.i("Solicitud cambio color", "Se quiere cambiar " + selectedBar + " a " + selectedColor)
-
+        when (selectedBar) {
+            "H1 (20%)" -> when (selectedColor) {
+                "Blanco" -> horizontalBars[0].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_white_20))
+                "Rojo" -> horizontalBars[0].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_red_20))
+                "Naranja" -> horizontalBars[0].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_orange_20))
+                "Amarillo" -> horizontalBars[0].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_yellow_20))
+                "Verde" -> horizontalBars[0].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_green_20))
+                "Cián" -> horizontalBars[0].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_cyan_20))
+                "Azul" -> horizontalBars[0].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_blue_20))
+                "Morado" -> horizontalBars[0].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_purple_20))
+                "Negro" -> horizontalBars[0].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_black_20))
+            }
+            "H2 (50%)" -> when (selectedColor) {
+                "Blanco" -> horizontalBars[1].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_white_50))
+                "Rojo" -> horizontalBars[1].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_red_50))
+                "Naranja" -> horizontalBars[1].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_orange_50))
+                "Amarillo" -> horizontalBars[1].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_yellow_50))
+                "Verde" -> horizontalBars[1].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_green_50))
+                "Cián" -> horizontalBars[1].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_cyan_50))
+                "Azul" -> horizontalBars[1].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_blue_50))
+                "Morado" -> horizontalBars[1].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_purple_50))
+                "Negro" -> horizontalBars[1].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_black_50))
+            }
+            "H3 (80%)" -> when (selectedColor) {
+                "Blanco" -> horizontalBars[2].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_white_80))
+                "Rojo" -> horizontalBars[2].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_red_80))
+                "Naranja" -> horizontalBars[2].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_orange_80))
+                "Amarillo" -> horizontalBars[2].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_yellow_80))
+                "Verde" -> horizontalBars[2].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_green_80))
+                "Cián" -> horizontalBars[2].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_cyan_80))
+                "Azul" -> horizontalBars[2].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_blue_80))
+                "Morado" -> horizontalBars[2].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_purple_80))
+                "Negro" -> horizontalBars[2].setCardBackgroundColor(ContextCompat.getColor(this, R.color.cm_black_80))
+            }
+            "V1 (20%)" -> when (selectedColor) {
+                "Blanco" -> verticalBars[0].color = ContextCompat.getColor(this, R.color.cm_white_20)
+                "Rojo" -> verticalBars[0].color = ContextCompat.getColor(this, R.color.cm_red_20)
+                "Naranja" -> verticalBars[0].color = ContextCompat.getColor(this, R.color.cm_orange_20)
+                "Amarillo" -> verticalBars[0].color = ContextCompat.getColor(this, R.color.cm_yellow_20)
+                "Verde" -> verticalBars[0].color = ContextCompat.getColor(this, R.color.cm_green_20)
+                "Cián" -> verticalBars[0].color = ContextCompat.getColor(this, R.color.cm_cyan_20)
+                "Azul" -> verticalBars[0].color = ContextCompat.getColor(this, R.color.cm_blue_20)
+                "Morado" -> verticalBars[0].color = ContextCompat.getColor(this, R.color.cm_purple_20)
+                "Negro" -> verticalBars[0].color = ContextCompat.getColor(this, R.color.cm_black_20)
+            }
+            "V2 (35%)" -> when (selectedColor) {
+                "Blanco" -> verticalBars[1].color = ContextCompat.getColor(this, R.color.cm_white_35)
+                "Rojo" -> verticalBars[1].color = ContextCompat.getColor(this, R.color.cm_red_35)
+                "Naranja" -> verticalBars[1].color = ContextCompat.getColor(this, R.color.cm_orange_35)
+                "Amarillo" -> verticalBars[1].color = ContextCompat.getColor(this, R.color.cm_yellow_35)
+                "Verde" -> verticalBars[1].color = ContextCompat.getColor(this, R.color.cm_green_35)
+                "Cián" -> verticalBars[1].color = ContextCompat.getColor(this, R.color.cm_cyan_35)
+                "Azul" -> verticalBars[1].color = ContextCompat.getColor(this, R.color.cm_blue_35)
+                "Morado" -> verticalBars[1].color = ContextCompat.getColor(this, R.color.cm_purple_35)
+                "Negro" -> verticalBars[1].color = ContextCompat.getColor(this, R.color.cm_black_35)
+            }
+            "V3 (50%)" -> when (selectedColor) {
+                "Blanco" -> verticalBars[2].color = ContextCompat.getColor(this, R.color.cm_white_50)
+                "Rojo" -> verticalBars[2].color = ContextCompat.getColor(this, R.color.cm_red_50)
+                "Naranja" -> verticalBars[2].color = ContextCompat.getColor(this, R.color.cm_orange_50)
+                "Amarillo" -> verticalBars[2].color = ContextCompat.getColor(this, R.color.cm_yellow_50)
+                "Verde" -> verticalBars[2].color = ContextCompat.getColor(this, R.color.cm_green_50)
+                "Cián" -> verticalBars[2].color = ContextCompat.getColor(this, R.color.cm_cyan_50)
+                "Azul" -> verticalBars[2].color = ContextCompat.getColor(this, R.color.cm_blue_50)
+                "Morado" -> verticalBars[2].color = ContextCompat.getColor(this, R.color.cm_purple_50)
+                "Negro" -> verticalBars[2].color = ContextCompat.getColor(this, R.color.cm_black_50)
+            }
+            "V4 (65%)" -> when (selectedColor) {
+                "Blanco" -> verticalBars[3].color = ContextCompat.getColor(this, R.color.cm_white_65)
+                "Rojo" -> verticalBars[3].color = ContextCompat.getColor(this, R.color.cm_red_65)
+                "Naranja" -> verticalBars[3].color = ContextCompat.getColor(this, R.color.cm_orange_65)
+                "Amarillo" -> verticalBars[3].color = ContextCompat.getColor(this, R.color.cm_yellow_65)
+                "Verde" -> verticalBars[3].color = ContextCompat.getColor(this, R.color.cm_green_65)
+                "Cián" -> verticalBars[3].color = ContextCompat.getColor(this, R.color.cm_cyan_65)
+                "Azul" -> verticalBars[3].color = ContextCompat.getColor(this, R.color.cm_blue_65)
+                "Morado" -> verticalBars[3].color = ContextCompat.getColor(this, R.color.cm_purple_65)
+                "Negro" -> verticalBars[3].color = ContextCompat.getColor(this, R.color.cm_black_65)
+            }
+            "V5 (80%)" -> when (selectedColor) {
+                "Blanco" -> verticalBars[4].color = ContextCompat.getColor(this, R.color.cm_white_80)
+                "Rojo" -> verticalBars[4].color = ContextCompat.getColor(this, R.color.cm_red_80)
+                "Naranja" -> verticalBars[4].color = ContextCompat.getColor(this, R.color.cm_orange_80)
+                "Amarillo" -> verticalBars[4].color = ContextCompat.getColor(this, R.color.cm_yellow_80)
+                "Verde" -> verticalBars[4].color = ContextCompat.getColor(this, R.color.cm_green_80)
+                "Cián" -> verticalBars[4].color = ContextCompat.getColor(this, R.color.cm_cyan_80)
+                "Azul" -> verticalBars[4].color = ContextCompat.getColor(this, R.color.cm_blue_80)
+                "Morado" -> verticalBars[4].color = ContextCompat.getColor(this, R.color.cm_purple_80)
+                "Negro" -> verticalBars[4].color = ContextCompat.getColor(this, R.color.cm_black_80)
+            }
+        }
+        rvColors.adapter?.notifyDataSetChanged()
     }
 
 }
