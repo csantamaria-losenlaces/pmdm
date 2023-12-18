7edc30d90902e1453db47cc1764ff32bc8f85061
CarlosSantamaria <csantamaria.losenlaces@gmail.com>
2023-12-18 19:27:21 +0100

    Terminada subaplicación Superheroes

--- app/src/main/java/com/csantamaria/appConMenu/superhero/DetailSuperheroActivity.kt
+++ app/src/main/java/com/csantamaria/appConMenu/superhero/DetailSuperheroActivity.kt
@@ -1,20 +1,30 @@
 package com.csantamaria.appConMenu.superhero
 
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.csantamaria.appConMenu.superhero.SuperheroListActivity.Companion.EXTRA_ID
 import com.example.appmensajeria.R
+import com.example.appmensajeria.databinding.ActivityDetailSuperheroBinding
+import com.squareup.picasso.Picasso
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.math.roundToInt
 
 class DetailSuperheroActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityDetailSuperheroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_superhero)
+
+        binding = ActivityDetailSuperheroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         val id: String = intent.getStringExtra(EXTRA_ID).orEmpty()
         getSuperheroInformation(id)
     }
@@ -23,6 +33,9 @@ class DetailSuperheroActivity : AppCompatActivity() {
         CoroutineScope(Dispatchers.IO).launch {
             val superheroDetail =
                 getRetrofit().create(ApiService::class.java).getSuperheroDetail(id)
+            if(superheroDetail.body() != null){
+                runOnUiThread { createUI(superheroDetail.body()!!) }
+            }
         }
     }
 
@@ -32,6 +45,33 @@ class DetailSuperheroActivity : AppCompatActivity() {
             .baseUrl("https://superheroapi.com/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+    }
+
+    private fun createUI(superhero: SuperheroDetailResponse) {
+        Picasso.get().load(superhero.image.url).into(binding.ivSuperhero)
+        binding.tvSuperheroName.text = superhero.name
+        binding.tvSuperheroRealName.text = superhero.biography.fullName
+        binding.tvPublisher.text = superhero.biography.publisher
+        prepareStats(superhero.powerstats)
+    }
+
+    private fun prepareStats(powerstats: PowerStatsResponse) {
+        updateHeight(binding.viewIntelligence, powerstats.intelligence)
+        updateHeight(binding.viewStrength, powerstats.strength)
+        updateHeight(binding.viewSpeed, powerstats.speed)
+        updateHeight(binding.viewDurability, powerstats.durability)
+        updateHeight(binding.viewPower, powerstats.power)
+        updateHeight(binding.viewCombat, powerstats.combat)
+    }
+
+    private fun updateHeight(view: View, stat:String){
+        val params = view.layoutParams
+        params.height = pxToDp(stat.toFloat())
+        view.layoutParams = params
+    }
+
+    private fun pxToDp(px:Float):Int{
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, px, resources.displayMetrics).roundToInt()
     }
 
 }
